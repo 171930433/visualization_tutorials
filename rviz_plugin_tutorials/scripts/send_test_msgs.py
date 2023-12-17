91d8715bdc5ff9fd0c85c31062c1ec96e94c3bc9
gsk <171930433@qq.com>
2023-12-17 10:28:09 +0800

    edit demo scripts

--- rviz_plugin_tutorials/scripts/send_test_msgs.py
+++ rviz_plugin_tutorials/scripts/send_test_msgs.py
@@ -16,6 +16,8 @@ rate = rospy.Rate(10)
 radius = 5
 angle = 0
 dir = 1
+px = 0
+py = 0
 
 dist = 3
 while not rospy.is_shutdown():
@@ -28,18 +30,28 @@ while not rospy.is_shutdown():
     imu.linear_acceleration.y = sin( angle )
     imu.linear_acceleration.z = sin( 10 )
 
+    # 临时存储位置
+    imu.angular_velocity.x = px
+    imu.angular_velocity.y = py
+    imu.angular_velocity.z = 0
+
     publisher.publish( imu )
 
-    br.sendTransform((0, 0, 0),
+
+
+    br.sendTransform((px, py, 0),
                      tf.transformations.quaternion_from_euler(0, 0, 0),
                      rospy.Time.now(),
                      "base_link",
                      "map")
     angle += dir * 0.01
-    if angle >= 1: 
-        dir = -1
-    if angle < 0:
-        dir = 1
+    if angle >= 3.14: 
+        dir = -3.14
+    if angle < -3.14:
+        dir = 3.14
+
+    px = radius * cos(angle)
+    py = radius * sin(angle)
 
     rate.sleep()
 
