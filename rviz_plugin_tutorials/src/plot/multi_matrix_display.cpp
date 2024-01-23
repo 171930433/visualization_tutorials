8c24c2c93cf038d25eece9bcac5b62c73ff2cb75
guanshoukui <guanshoukui@zhito.com>
2024-01-23 13:26:45 +0800

    搭建MultiMatrixDisplay 框架

---
+++ rviz_plugin_tutorials/src/plot/multi_matrix_display.cpp
@@ -0,0 +1,229 @@
+#include <rviz/properties/int_property.h>
+#include <rviz/properties/enum_property.h>
+#include <rviz/properties/bool_property.h>
+#include <rviz/properties/color_property.h>
+#include <rviz/properties/string_property.h>
+#include <rviz/visualization_manager.h>
+
+#include "plot/multi_matrix_display.h"
+#include "plot/matrix_widget.h"
+#include "protobuf_helper.h"
+
+int MultiMatrixDisplay::object_count_ = 0;
+QString MultiMatrixDisplay::generateName()
+{
+  return QString("MultiMatrixDisplay-p%1").arg(object_count_);
+}
+
+MultiMatrixDisplay::MultiMatrixDisplay()
+{
+  InitPersons();
+  setClassId("rviz_plugin_tutorials/MultiMatrixDisplay"); // 和描述文件得一致
+  view_ = new MatrixWidget();
+  view_->setDisplaySync(this);
+
+  //
+  counts_prop_ = new rviz::IntProperty("channel counts", 0, "the number of channel counts", this, SLOT(UpdateChannelCount()));
+  counts_prop_->setMin(1);
+  counts_prop_->setMax(0);
+
+  // ! 需要互相访问,限制范围的set需要在row col都构造完再设置
+  row_prop_ = new rviz::IntProperty("row count", 0, "row of mat graphs", this, SLOT(UpdateRow()));
+  col_prop_ = new rviz::IntProperty("col count", 0, "col of mat graphs", this, SLOT(UpdateCol()));
+
+  // 会自动生成rect-0-0
+  row_prop_->setMin(1);
+  row_prop_->setMax(10);
+  col_prop_->setMin(1);
+  col_prop_->setMax(10);
+  ++object_count_;
+}
+
+MultiMatrixDisplay::~MultiMatrixDisplay()
+{
+  if (initialized())
+  {
+    delete view_;
+  }
+  --object_count_;
+}
+
+void MultiMatrixDisplay::UpdateChannelCount()
+{
+
+  int const new_count = counts_prop_->getInt();
+  int const old_count = data_channels_.size();
+
+  if (new_count < old_count) // 删除元素
+  {
+    for (int i = new_count; i < old_count; ++i)
+    {
+      this->takeChild(data_channels_.back().get());
+      data_channels_.pop_back();
+    }
+  }
+  else
+  {
+    for (int i = old_count; i < new_count; ++i)
+    {
+      QString channel_header = (data_channels_.size() == 0 ? QString("main_channel") : QString("data_channel-%1").arg(data_channels_.size()));
+      auto single_changel = std::make_shared<rviz::EnumProperty>(channel_header, "", "data_channel", counts_prop_, SLOT(UpdateChannel()));
+      connect(single_changel.get(), &rviz::EnumProperty::requestOptions, this, &MultiMatrixDisplay::ListCurrentChannel);
+      data_channels_.push_back(single_changel);
+    }
+  }
+}
+
+void MultiMatrixDisplay::ListCurrentChannel(rviz::EnumProperty *topics)
+{
+  auto const names = g_cacher_->GetChannelNames();
+  topics->clearOptions();
+  for (auto const &name : names)
+  {
+    topics->addOptionStd(name);
+    // qDebug() << "name = " << QString::fromStdString(name);
+  }
+
+  qDebug() << QString("MultiMatrixDisplay::ListCurrentChannel() called ,size= %1 ").arg(names.size());
+}
+
+void MultiMatrixDisplay::UpdateRow()
+{
+  int const old_row = fields_prop_.rows();
+  int const new_row = row_prop_->getInt();
+  int const col = col_prop_->getInt();
+  if (col == 0)
+  {
+    return;
+  }
+
+  // 增加
+  if (old_row < new_row)
+  {
+    fields_prop_.conservativeResize(new_row, col);
+    for (int i = old_row; i < new_row; ++i)
+    {
+      for (int j = 0; j < col; ++j)
+      {
+        fields_prop_(i, j) = new rviz::StringProperty(QString("rect-%1-%2").arg(i).arg(j), "", "matrix plot field", this);
+        connect(fields_prop_(i, j), &Property::changed, [this, i, j]()
+                { this->UpdateFieldName(i, j); });
+      }
+    }
+  }
+  else // 删除
+  {
+    for (int i = new_row; i < old_row; ++i)
+    {
+      for (int j = 0; j < col; ++j)
+      {
+        this->takeChild(fields_prop_(i, j));
+        delete fields_prop_(i, j);
+      }
+    }
+    fields_prop_.conservativeResize(new_row, col); // 删除操作
+  }
+  qDebug() << QString("col_prop_->getInt()=%1*%2").arg(new_row).arg(col_prop_->getInt());
+  view_->UpdatePlotLayout(new_row, col);
+}
+void MultiMatrixDisplay::UpdateCol()
+{
+
+  int const old_col = fields_prop_.cols();
+  int const new_col = col_prop_->getInt();
+  int const row = row_prop_->getInt();
+  if (row == 0)
+  {
+    return;
+  }
+  // 增加
+  if (old_col < new_col)
+  {
+    fields_prop_.conservativeResize(row, new_col);
+    for (int i = 0; i < row; ++i)
+    {
+      for (int j = old_col; j < new_col; ++j)
+      {
+        fields_prop_(i, j) = new rviz::StringProperty(QString("rect-%1-%2").arg(i).arg(j), "", "matrix plot field", this);
+        connect(fields_prop_(i, j), &Property::changed, [this, i, j]()
+                { this->UpdateFieldName(i, j); });
+      }
+    }
+  }
+  else // 删除
+  {
+    for (int i = 0; i < row; ++i)
+    {
+      for (int j = new_col; j < old_col; ++j)
+      {
+        this->takeChild(fields_prop_(i, j));
+        delete fields_prop_(i, j);
+      }
+    }
+    fields_prop_.conservativeResize(row, new_col); // 删除操作
+  }
+  view_->UpdatePlotLayout(row, new_col);
+}
+
+void MultiMatrixDisplay::UpdateFieldName(int const row, int const col)
+{
+  QString const &field_name = fields_prop_(row, col)->getString();
+  if (field_name == "")
+  {
+    return;
+  }
+  view_->UpdateFieldName(row, col, field_name);
+}
+
+void MultiMatrixDisplay::CreateMatrixPlot(QString const &name, MatrixXQString const &field_names)
+{
+  view_->CreatePlot(name, field_names);
+  // 创建property
+  row_prop_->setInt(field_names.rows());
+  col_prop_->setInt(field_names.cols());
+
+  for (int i = 0; i < field_names.rows(); ++i)
+  {
+    for (int j = 0; j < field_names.cols(); ++j)
+    {
+      fields_prop_(i, j)->setString(field_names(i, j));
+    }
+  }
+}
+
+// Overrides from Display
+void MultiMatrixDisplay::onInitialize()
+{
+  setAssociatedWidget(view_);
+}
+
+void MultiMatrixDisplay::update(float dt, float ros_dt)
+{
+}
+
+void MultiMatrixDisplay::load(const rviz::Config &config)
+{
+  int row = 0;
+  if (config.mapGetInt("row count", &row))
+  {
+    row_prop_->setInt(row);
+  }
+  int col = 0;
+  if (config.mapGetInt("col count", &col))
+  {
+    col_prop_->setInt(col);
+  }
+  rviz::Display::load(config);
+}
+void MultiMatrixDisplay::save(rviz::Config config) const
+{
+  rviz::Display::save(config);
+}
+
+// void MultiMatrixDisplay::AddSeries(QString const &name, QStringList const &field_names)
+// {
+//   view_->AddSeries(name, field_names);
+// }
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(MultiMatrixDisplay, rviz::Display)
---
+++ rviz_plugin_tutorials/src/plot/multi_matrix_display.h
@@ -0,0 +1,69 @@
+#pragma once
+
+// #include <rviz/properties/color_property.h>
+// #include <rviz/properties/float_property.h>
+#include <rviz/properties/bool_property.h>
+#include <rviz/properties/int_property.h>
+
+// #include <rviz/properties/vector_property.h>
+// #include <rviz/properties/enum_property.h>
+// #include <rviz/properties/tf_frame_property.h>
+#include <rviz/display.h>
+
+#include "display_sync_base.h"
+#include "trajectory_widget.h"
+#include <deque>
+#include <eigen3/Eigen/Dense>
+
+namespace rviz
+{
+  class IntProperty;
+  class StringProperty;
+  class EnumProperty;
+  class BoolProperty;
+  class GroupProperty;
+  class ColorProperty;
+}
+
+using MatrixXQStringProp = Eigen::Matrix<rviz::StringProperty *, Eigen::Dynamic, Eigen::Dynamic>;
+
+class MatrixWidget;
+// class QCPCurve;
+
+class MultiMatrixDisplay : public DisplaySyncBase
+{
+  Q_OBJECT
+public:
+  MultiMatrixDisplay();
+  ~MultiMatrixDisplay() override;
+  static QString generateName(); // 防止用户重建时名称重复
+
+public:
+  // void AddSeries(QString const &name, QStringList const &field_names);
+  void CreateMatrixPlot(QString const &name, MatrixXQString const &field_names);
+
+  // Overrides from Display
+  virtual void load(const rviz::Config &config);
+  virtual void save(rviz::Config config) const;
+  void onInitialize() override;
+  void update(float dt, float ros_dt) override;
+
+private Q_SLOTS:
+  void UpdateFieldName(int const row, int const col);
+  void UpdateRow();
+  void UpdateCol();
+  void UpdateChannelCount();
+  void ListCurrentChannel(rviz::EnumProperty *topics);
+
+private:
+  MatrixWidget *view_ = nullptr;
+
+  MatrixXQStringProp fields_prop_;
+  rviz::IntProperty *row_prop_ = nullptr;
+  rviz::IntProperty *col_prop_ = nullptr;
+  rviz::IntProperty *counts_prop_ = nullptr; // 通道数目
+
+private:
+  static int object_count_;
+  std::deque<std::shared_ptr<rviz::EnumProperty>> data_channels_;
+};
