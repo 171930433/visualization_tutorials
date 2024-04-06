3ec63b2712e06bb58a99005027381667b402b7ea
gsk <171930433@qq.com>
2024-04-06 14:45:53 +0800

    最小的带交互的三维场景完成

--- librviz_tutorial/src/myviz.cpp
+++ librviz_tutorial/src/myviz.cpp
@@ -34,42 +34,46 @@
 #include <QLabel>
 #include <QSlider>
 #include <QVBoxLayout>
+#include <QDebug>
+#include <QMouseEvent>
 
 #include <memory>
+
+#include <OgreCamera.h>
 
 #include "rviz_common/display.hpp"
 #include "rviz_common/render_panel.hpp"
 #include "rviz_common/ros_integration/ros_node_abstraction.hpp"
+#include "rviz_common/view_manager.hpp"
+#include "rviz_common/tool_manager.hpp"
 #include "rviz_common/visualization_manager.hpp"
 #include "rviz_common/window_manager_interface.hpp"
 #include "rviz_rendering/render_window.hpp"
 
 // BEGIN_TUTORIAL
 // Constructor for MyViz.  This does most of the work of the class.
-MyViz::MyViz(
-  QApplication * app,
-  rviz_common::ros_integration::RosNodeAbstractionIface::WeakPtr rviz_ros_node,
-  QWidget * parent)
-: QMainWindow(parent), app_(app), rviz_ros_node_(rviz_ros_node)
-{
+MyViz::MyViz(QApplication *app,
+             rviz_common::ros_integration::RosNodeAbstractionIface::WeakPtr rviz_ros_node,
+             QWidget *parent)
+    : QMainWindow(parent), app_(app), rviz_ros_node_(rviz_ros_node) {
   // Construct and lay out labels and slider controls.
-  QLabel * thickness_label = new QLabel("Line Thickness");
-  QSlider * thickness_slider = new QSlider(Qt::Horizontal);
+  QLabel *thickness_label = new QLabel("Line Thickness");
+  QSlider *thickness_slider = new QSlider(Qt::Horizontal);
   thickness_slider->setMinimum(1);
   thickness_slider->setMaximum(100);
-  QLabel * cell_size_label = new QLabel("Cell Size");
-  QSlider * cell_size_slider = new QSlider(Qt::Horizontal);
+  QLabel *cell_size_label = new QLabel("Cell Size");
+  QSlider *cell_size_slider = new QSlider(Qt::Horizontal);
   cell_size_slider->setMinimum(1);
   cell_size_slider->setMaximum(100);
-  QGridLayout * controls_layout = new QGridLayout();
+  QGridLayout *controls_layout = new QGridLayout();
   controls_layout->addWidget(thickness_label, 0, 0);
   controls_layout->addWidget(thickness_slider, 0, 1);
   controls_layout->addWidget(cell_size_label, 1, 0);
   controls_layout->addWidget(cell_size_slider, 1, 1);
 
   // Add visualization.
-  QVBoxLayout * main_layout = new QVBoxLayout;
-  QWidget * central_widget = new QWidget();
+  QVBoxLayout *main_layout = new QVBoxLayout;
+  QWidget *central_widget = new QWidget(this);
   main_layout->addLayout(controls_layout);
   main_layout->setSpacing(0);
   main_layout->setMargin(0);
@@ -81,17 +85,22 @@ MyViz::MyViz(
   // very central and we will probably need one in every usage of
   // librviz.
   app_->processEvents();
-  render_panel_ = std::make_shared<rviz_common::RenderPanel>(central_widget);
+  // render_panel_ = std::make_shared<rviz_common::RenderPanel>(central_widget);
+  render_panel_ = std::make_shared<rviz_common::RenderPanel>();
   app_->processEvents();
   render_panel_->getRenderWindow()->initialize();
   auto clock = rviz_ros_node_.lock()->get_raw_node()->get_clock();
-  rviz_common::WindowManagerInterface * wm = nullptr;
-  manager_ = std::make_shared<rviz_common::VisualizationManager>(
-    render_panel_.get(), rviz_ros_node_, wm, clock);
+  rviz_common::WindowManagerInterface *wm = nullptr;
+  manager_ = std::make_shared<rviz_common::VisualizationManager>(render_panel_.get(), rviz_ros_node_, wm, clock);
+
   render_panel_->initialize(manager_.get());
   app_->processEvents();
   manager_->initialize();
   manager_->startUpdate();
+
+  manager_->getToolManager()->addTool("rviz_default_plugins/MoveCamera");
+  manager_->getViewManager()->setCurrentViewControllerType("rviz_default_plugins/TopDownOrtho");
+  manager_->getViewManager()->getCurrent()->getCamera()->setPosition(0, 0, 10);
 
   // Set the top-level layout for this MyViz widget.
   central_widget->setLayout(main_layout);
@@ -104,9 +113,7 @@ MyViz::MyViz(
 
   // Create a Grid display.
   grid_ = manager_->createDisplay("rviz_default_plugins/Grid", "adjustable grid", true);
-  if (grid_ == NULL) {
-    throw std::runtime_error("Error creating grid display");
-  }
+  if (grid_ == NULL) { throw std::runtime_error("Error creating grid display"); }
 
   // Configure the GridDisplay the way we like it.
   grid_->subProp("Line Style")->setValue("Billboards");
@@ -119,24 +126,17 @@ MyViz::MyViz(
 
 // This function is a Qt slot connected to a QSlider's valueChanged() signal.
 // It sets the line thickness of the grid by changing the grid's "Line Width" property.
-void MyViz::setThickness(int thickness_percent)
-{
-  if (grid_ != NULL) {
-    grid_->subProp("Line Style")->subProp("Line Width")->setValue(thickness_percent / 100.0f);
-  }
+void MyViz::setThickness(int thickness_percent) {
+  if (grid_ != NULL) { grid_->subProp("Line Style")->subProp("Line Width")->setValue(thickness_percent / 100.0f); }
 }
 
 // This function is a Qt slot connected to a QSlider's valueChanged() signal.
 // It sets the cell size of the grid by changing the grid's "Cell Size" Property.
-void MyViz::setCellSize(int cell_size_percent)
-{
-  if (grid_ != NULL) {
-    grid_->subProp("Cell Size")->setValue(cell_size_percent / 10.0f);
-  }
+void MyViz::setCellSize(int cell_size_percent) {
+  if (grid_ != NULL) { grid_->subProp("Cell Size")->setValue(cell_size_percent / 10.0f); }
 }
 
-void MyViz::closeEvent(QCloseEvent * event)
-{
+void MyViz::closeEvent(QCloseEvent *event) {
   QMainWindow::closeEvent(event);
   rclcpp::shutdown();
 }
